99c1f66e08038795c2d0f8607bd34060c6b17350
Marius Köhler <mariuskoehler@yahoo.de>
2024-05-17 10:29:26 +0200

    openapi / contecst update: jwt

--- src/core/openapi/model/models.ts
+++ src/core/openapi/model/models.ts
@@ -1,6 +1,6 @@
 export * from './collectionPath';
 export * from './document';
+export * from './loginUser200Response';
 export * from './userCandidate';
 export * from './userCredentials';
 export * from './userRole';
-export * from './userSession';
