3038c8863e287d5992033fd2eabeb692b163da62
Marius Köhler <mariuskoehler@yahoo.de>
2024-06-10 13:38:20 +0200

    apiKey reset

--- src/classes/auth-interceptor.ts
+++ src/classes/auth-interceptor.ts
@@ -1,8 +1,8 @@
 import {
-    HttpEvent,
-    HttpHandler,
-    HttpInterceptor,
-    HttpRequest,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -11,13 +11,15 @@ export class AuthInterceptor implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    const apiKey =
+      'eyJhbGciOiJIUzI1NiJ9.NWUwN2QyZDU5YmIwMDViNTlkZjM4ZjIyOTljNjdiZGI0YzAwZDg2OWE5M2Q1YWY3MDhiZWRhMDRhZTMwMTU2OTFmODRmMDM3OTYwOTY0Zjk5ZjQxMzZiODRlNWFiMWUzMjg1NDliZmJkZGRkZjgxYTU5MWU5NjY1MGZjZTA0OWM.yu3GV7ZfYBtnwp62UB1tBvqtSh3-GxqHNRQUUIYJcZg';
     const token = localStorage.getItem('access_token');
 
     if (token) {
       return next.handle(
         req.clone({
           setHeaders: {
-            Authorization: `Bearer ${process.env['API_KEY']},${token}`,
+            Authorization: `Bearer ${apiKey},${token}`,
           },
         })
       );
