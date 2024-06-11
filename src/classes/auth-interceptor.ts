9d35ecfb3d267b7fac33221d0e6fb79f97cfc9ac
Marius Köhler <mariuskoehler@yahoo.de>
2024-06-11 09:20:49 +0200

    api key rollback

--- src/classes/auth-interceptor.ts
+++ src/classes/auth-interceptor.ts
@@ -11,15 +11,13 @@ export class AuthInterceptor implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    const apiKey =
-      'eyJhbGciOiJIUzI1NiJ9.NWUwN2QyZDU5YmIwMDViNTlkZjM4ZjIyOTljNjdiZGI0YzAwZDg2OWE5M2Q1YWY3MDhiZWRhMDRhZTMwMTU2OTFmODRmMDM3OTYwOTY0Zjk5ZjQxMzZiODRlNWFiMWUzMjg1NDliZmJkZGRkZjgxYTU5MWU5NjY1MGZjZTA0OWM.yu3GV7ZfYBtnwp62UB1tBvqtSh3-GxqHNRQUUIYJcZg';
     const token = localStorage.getItem('access_token');
 
     if (token) {
       return next.handle(
         req.clone({
           setHeaders: {
-            Authorization: `Bearer ${apiKey},${token}`,
+            Authorization: `Bearer ${token}`,
           },
         })
       );
